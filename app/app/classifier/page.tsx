f3a89a68ff91e03f860c956ea2898d97a3224670
knevagi <43566634+knevagi@users.noreply.github.com>
2024-12-28 20:27:45 +0530

    change text colour to black and removed unnecessary code

--- app/app/classifier/page.tsx
+++ app/app/classifier/page.tsx
@@ -1,36 +1,3 @@
-// 'use client'
-// import React,{useState,Suspense} from 'react'
-// import { useRouter,useSearchParams } from 'next/navigation'
-// import ClassTable from '../components/ClassTable'
-// import FileUpload from '../components/FileUpload'
-
-// const ClassifierDetails = () => {
-//   const router = useSearchParams();
-//   const type=router.get("name")|| "Default";;
-//   const imgurl=type=="Flower"?"\\assets\\img\\flower.jpg":"\\assets\\img\\burger.jpg";
-//   const heading=type=="Flower"?"Flower Classifier!":"Food Classifier!";
-//   const subheading=type=="Flower"?"Discover Nature's Beauty: Effortlessly Identify and Appreciate Flowers with AI's Help.":"Embark on Culinary Journeys: Instantly Recognize Your Favorite Foods with AI Precision.";
-//   return (
-//     <Suspense fallback={<div>Loading...</div>}>
-//     <div>
-//     <div className="hero min-h-56 bg-base-200">
-//       <div className="hero-content flex-col lg:flex-row-reverse">
-//         <img src={imgurl} className="max-w-sm rounded-lg shadow-2xl" />
-//         <div>
-//           <h1 className="text-5xl font-bold">{heading}</h1>
-//           <p className="py-6">{subheading}</p>
-//         </div>
-//       </div>
-//     </div>
-//     <FileUpload className={type} />
-//     <ClassTable classifiertype={type} />
-//   </div>
-//   </Suspense>
-    
-//   )
-// }
-
-// export default ClassifierDetails
 'use client'
 import React, { Suspense } from 'react'
 import ClassTable from '../components/ClassTable'
@@ -47,13 +14,13 @@ const ClassifierContent = () => {
     : "Embark on Culinary Journeys: Instantly Recognize Your Favorite Foods with AI Precision.";
 
   return (
-    <div>
+    <div className="text-black">
       <div className="hero min-h-56 bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={imgurl} className="max-w-sm rounded-lg shadow-2xl" alt={type + " image"} />
           <div>
-            <h1 className="text-5xl font-bold">{heading}</h1>
-            <p className="py-6">{subheading}</p>
+            <h1 className="text-5xl font-bold text-black">{heading}</h1>
+            <p className="py-6 text-black">{subheading}</p>
           </div>
         </div>
       </div>
@@ -65,7 +32,7 @@ const ClassifierContent = () => {
 
 const ClassifierDetails = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="text-black">Loading...</div>}>
       <ClassifierContent />
     </Suspense>
   )
